5647179d2fb60630d65eb74ff14eca57e157833d
Rajkoumar <sylvain.rajkoumar@gmail.com>
2018-12-15 17:10:31 +0100

    Ajout écran noir quand la capture est coupé

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import cv2
 from utils.object_tracking import ObjectTracking
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QDialog
+import numpy as np
 
 class Controller(object):
 
@@ -19,7 +20,11 @@ class Controller(object):
     def toggle_webcam_timer(self):
         if self.webcam_enabled:
             self.webcam_timer.stop()
+            blank = np.zeros([500,500,3])
+            self.view.updateView(blank)
+            self.capture.release()
         else:
+            self.capture.open(0)
             self.webcam_timer.start(40)
 
         self.webcam_enabled = not self.webcam_enabled
